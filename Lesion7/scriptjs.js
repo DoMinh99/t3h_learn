e5484ec72f66e184443cf3f2bdc6dfdee5c783d2
DoMinh99 <dohongminh2811999@gmail.com>
2024-08-10 00:07:05 +0700

    nháp thôi

---
+++ Lesion7/scriptjs.js
@@ -0,0 +1,14 @@
+// alert('hello world')
+
+var name = 't3h';
+// Khởi ạo một biến tên là "name"
+
+// console.log(name);
+
+// Những biến được khai báo bẳng từ khoá "let" hoặc "var" sẽ có đặc điểm như sau: 
+// biến đó có thẻ được khởi tạo mà chưa cần phải gán giá trị khởi tạo 
+// vd: let count
+
+let b1 = "baymuoi"
+let b2 = "bamuoi"
+console.log(b1+b2);
