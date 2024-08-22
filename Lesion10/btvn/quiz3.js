e4bcf4e451e4f9aa0eb9450d99e2c5d0dba24234
DoMinh99 <dohongminh2811999@gmail.com>
2024-08-22 16:23:53 +0700

    quiz3 bài 2

--- Lesion10/btvn/quiz3.js
+++ Lesion10/btvn/quiz3.js
@@ -13,3 +13,14 @@ function printSlogan(slogan) {
 
 // Sử dụng function
 printSlogan("Hãy sống trọn vẹn mỗi ngày!");
+
+//bài 2
+// Hãy sử dụng phương thức map để biến đổi 1 mảng texts bao gồm các chuỗi thành 1 mảng textsLength 
+// có các phần tử là chiều dài tương ứng của từng chuỗi trong mảng texts.
+
+const texts = ["hello", "world", "hongminh", "hocmai"];
+
+// Sử dụng map để tạo ra mảng textsLength chứa chiều dài của từng chuỗi
+const textsLength = texts.map(text => text.length);
+
+console.log(textsLength); // Output: [5, 5, 10, 6]
