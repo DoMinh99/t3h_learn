97b496d820a6e7f17f383d07d6333a9b4ef75b43
DoMinh99 <dohongminh2811999@gmail.com>
2024-08-22 15:56:56 +0700

    quiz2 bài 1

---
+++ Lesion10/btvn/quiz2.js
@@ -0,0 +1,9 @@
+// bài 1
+// Khai báo 1 function nhận đầu vào là chuỗi name, thực hiện in ra màn hình “Hello world, ” + name.
+
+
+function hehe(name){
+    console.log("hello world, " + name);
+}
+
+hehe("hồng minh");
