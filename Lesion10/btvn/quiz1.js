95bd0265d090fb8f157fe67bd8e7b8957bb28a36
DoMinh99 <dohongminh2811999@gmail.com>
2024-08-22 15:42:40 +0700

    bài 1 và 2

---
+++ Lesion10/btvn/quiz1.js
@@ -0,0 +1,37 @@
+
+//bài 1
+// Khai báo 1 đối tượng để mô tả bản thân bao gồm: tên, tuổi, địa chỉ, giới tính, 
+// sở thích (nhiều sở thích), chiều cao, cân nặng, tình trạng hôn nhân, số điện thoại.
+
+let doituong = {
+    ten: "do hong minh",
+    tuoi: 25,
+    diachi: "khoai chau, hung yen",
+    gioitinh: "nam",
+    sothich: ['nghe nhac', "xem tiktok", "choi game"],
+    chieucao: 172,
+    cannang: 60,
+    honnhan: "doc than",
+    sdt: "0936667383",
+
+};
+
+console.log(doituong);
+
+//bài 2
+// Khai báo 1 đối tượng là bạn trai/bạn gái của bạn: tên, tuổi, sự tốt bụng (1 - 10),
+//  nhà giàu (boolean).
+
+let bangai = {
+    ten: "thu hong",
+    tuoi: 24,
+    sutotbung: 9,
+    nhagiau: true,
+};
+
+console.log(bangai);    
+
+// bài 3
+// Viết chương trình kiểm tra nếu tuổi của bạn và bạn gái (trai) đều lớn hơn 20, 
+// in ra màn hình “Tầm này cưới được rồi”. Ngược lại, in ra “Chờ thêm chút nữa”
+
