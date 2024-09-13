961ba051c5d83112f3ff5a08cf9ea467d0a7403f
DoMinh99 <dohongminh2811999@gmail.com>
2024-09-13 19:23:03 +0700

    thêm vào giỏ hàng

---
+++ CRUD_Product/pages/list-products/scripts.js
@@ -0,0 +1,118 @@
+// -Json không phải kiểu dữ liệu, nó chỉ là định dạng dữ liệu
+// -Json là kiểu dữ liệu string
+// JSON.stringify(data) => chuyển đổi data sang định dạng json (kiểu dữ liệu string)
+// JSON.parse(data) => chuyển đổi data từ định dạng json về kiểu dữ liệu ban đầu
+
+const listProduct = JSON.parse(localStorage.getItem('listProduct')) || [];
+const cart = JSON.parse(localStorage.getItem('cart')) || [];
+// Các input
+const $idProduct = document.getElementById('id');
+const $titleProduct = document.getElementById('title');
+const $priceProduct = document.getElementById('price');
+const $imageProduct = document.getElementById('image');
+const $descriptionProduct = document.getElementById('description');
+// Các button
+const $btnCreateProduct = document.getElementById('create-product');
+
+// Bước 1: gán event cho button thêm mới
+$btnCreateProduct.addEventListener('click', function () {
+	// Trong hàm
+	// Bước 2: lấy các thông tin người dùng đã nhập ở các ô input
+	const id = $idProduct.value;
+	const title = $titleProduct.value;
+	const price = $priceProduct.value;
+	const image = $imageProduct.value;
+	const description = $descriptionProduct.value;
+	// Bước 3: kiểm tra thông tin (optional)
+	// Tự làm
+	// Bước 4: tạo 1 object mới (tương đương với tạo 1 sản phẩm mới) chứa các thông tin người dùng vừa nhập
+	const newProduct = {
+		id: Number(id),
+		title: title,
+		price: Number(price),
+		image: image,
+		description: description,
+	};
+	// Bước 5: thêm sản phẩm vừa tạo vào trong danh sách sản phẩm đã có
+	listProduct.push(newProduct);
+	// Bước 6: sau khi tạo thành công thì sẽ reset input
+	$idProduct.value = '';
+	$titleProduct.value = '';
+	$priceProduct.value = '';
+	$imageProduct.value = '';
+	$descriptionProduct.value = '';
+	// Bước 7: lưu thông tin danh sách sản phẩm mới nhất vào trong local storage
+	localStorage.setItem('listProduct', JSON.stringify(listProduct));
+	// Bước 8: gọi lại hàm renderListProduct để in thông tin danh sách sản phẩm mới nhất
+	renderListProduct();
+	console.log(listProduct);
+});
+
+function renderListProduct() {
+	let htmlProducts = '';
+
+	for (let product of listProduct) {
+		htmlProducts += `
+                <div class="col-3 mt-3" style="padding: 0px 12px">
+					<div class="card" style="width: 18rem">
+						<img
+							src="${product.image}"
+							class="card-img-top"
+							alt="${product.title}"
+                            style="width: 280px; height: 280px; object-fit: contain;"
+						/>
+						<div class="card-body">
+							<h5 class="card-title">${product.title}</h5>
+							<p class="card-text mt-1 mb-1">${product.price} VND</p>
+							<p class="card-text">${product.description}</p>
+							<div>
+								<button class="btn btn-primary">Xem chi tiết</button>
+								<button onclick="addToCart(${product.id})" type="button" class="btn btn-secondary" style="margin-left: 8px"><i class="fa-solid fa-cart-plus"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+            `;
+	}
+
+	document.getElementById('container-products').innerHTML = htmlProducts;
+}
+
+renderListProduct();
+
+// Bước 1: Gán function cho button add to cart
+function addToCart(productId) {
+	let productFind = null;
+	// Bước 2: Khi function được gọi =>
+	// xử lý để lấy được thông tin sản phẩm mà người dùng click
+	for (let product of listProduct) {
+		if (product.id === productId) {
+			productFind = product;
+		}
+	}
+	// Bước 3: Xử lý thông tin sản phẩm
+	let indexProduct = -1;
+	for (let i = 0; i < cart.length; i++) {
+		if (productFind.id === cart[i].id) {
+			indexProduct = i;
+		}
+	}
+	// TH1: Nếu trong giỏ hàng chưa tồn tại sản phẩm
+	// 	=> Thêm mới sản phẩm vào trong giỏ hàng
+	if (indexProduct === -1) {
+		const newProduct = {
+			id: productFind.id,
+			title: productFind.title,
+			image: productFind.image,
+			price: productFind.price,
+			quantity: 1,
+		};
+		cart.push(newProduct);
+	} else {
+		// 	TH2: Nếu trong giỏ hàng đã tồn tại sản phẩm
+		// 	=> Tăng số lượng lên
+		cart[indexProduct].quantity = cart[indexProduct].quantity + 1;
+	}
+	// Bước 4: Sau khi xử lý dữ liệu => lưu thông tin vào trong local storage
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
