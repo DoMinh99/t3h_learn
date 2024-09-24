0dcc221e7db7af4201a35433551c853c7fbbd2f5
DoMinh99 <dohongminh2811999@gmail.com>
2024-09-24 20:32:01 +0700

    switch case

--- Lesion8_2/sxript.js
+++ Lesion8_2/sxript.js
@@ -17,13 +17,27 @@
 // Nếu điểm  < 6.5 và điểm >= 5.5 học trung bình
 // nếu  < 5.0 học lực yếu
 
-const score = 7.0;
-if(score >= 8){
-    console.log('học giỏi')
-}else if ( 6.5 <= score && score < 8){
-    console.log('học khá')
-}else if( score >= 5.0 && score < 6.5){
-    console.log('học trung bình')
-}else{
-    console.log('học yếu')
-}
+// const score = 7.0;
+// if(score >= 8){
+//     console.log('học giỏi')
+// }else if ( 6.5 <= score && score < 8){
+//     console.log('học khá')
+// }else if( score >= 5.0 && score < 6.5){
+//     console.log('học trung bình')
+// }else{
+//     console.log('học yếu')
+// }
+
+// bài 2: xác định giá trị được nhập
+
+let day = 9;
+    switch(day){
+        case 2: console.log('thứ hai'); break;
+        case 3: console.log('thứ ba'); break;
+        case 4: console.log('thứ tư');break;
+        case 5: console.log('thứ năm');break;
+        case 6: console.log('thứ sáu');break;
+        case 7: console.log('thứ bayr');break;
+        case 8: console.log('chủ nhật');break;
+        default: console.log('ngày không hợp lệ');break;
+    }
