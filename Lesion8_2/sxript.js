e84ab841a03940cd0875d5e0f1b344ce23112e9e
DoMinh99 <dohongminh2811999@gmail.com>
2024-09-24 21:45:41 +0700

    kiễm tra chẵn từ số nhập tùw bàn phím

--- Lesion8_2/sxript.js
+++ Lesion8_2/sxript.js
@@ -52,15 +52,26 @@
 // }
 
 //in ra màn hình các số chẵn từ -10 - 50
-for (let i = -10; i<= 50; i++){
-    if(i % 2 === 0 ){
-        console.log(i);
-    }
-}
-
-// tính tôneg các số từ  -10 - 50
 // for (let i = -10; i<= 50; i++){
 //     if(i % 2 === 0 ){
 //         console.log(i);
 //     }
 // }
+
+// tính tôneg các số từ  -10 - 50
+// let sum = 0;
+// for (let i = -10; i<= 50; i++){
+//     if(i % 2 === 0 ){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+
+// nhập 1 trị từ bàn phím, nếu là số chẵn thì chương trình kết thúc
+let number;
+do {
+    number = prompt('nhập số kiểm tra');
+    number = Number(number);
+} while(number % 2 ===1);
+console.log(number);
+console.log(typeof number);
