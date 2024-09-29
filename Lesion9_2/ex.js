c74e3f36f8de4ae38c2b3ca0fddfbaf683637811
DoMinh99 <dohongminh2811999@gmail.com>
2024-09-29 20:06:12 +0700

    update - duyệt mảng

--- Lesion9_2/ex.js
+++ Lesion9_2/ex.js
@@ -1,6 +1,27 @@
 //khai báo và khởi tạo mảng
 
-const arr = [];
-const arr2 = [1,2,3,4,5,6,];
+// const arr = [];
+// const arr2 = [1,2,3,4,5,6,];
 
-console.log(arr2);
+// console.log(arr2);
+
+//truy xuất và thay đổi giá trị 
+//truy xuất
+const fruits = ['apple', 'banana', 'sung'];
+console.log(fruits);
+
+console.log(fruits[0]);
+
+//thay đổi giá trị
+fruits[0] = 'táo tàu';
+console.log(fruits);
+
+//duyệt mảng
+for (let index = 0; index <fruits.length; index++){
+    console.log(fruits[index]);
+}
+
+console.log("duyệt mảng theo vòng lặp for of");
+for(let fruit of fruits){
+    console.log(fruit);
+}
