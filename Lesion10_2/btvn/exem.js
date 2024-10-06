5c6c9327748457648dff4b00250e9fbebdfd81ec
DoMinh99 <dohongminh2811999@gmail.com>
2024-10-06 19:16:08 +0700

    btvn

---
+++ Lesion10_2/btvn/exem.js
@@ -0,0 +1,47 @@
+// const obj1 = {};
+// const obj2 = {
+//     Name : "minh",
+//     address : "hungw yên"
+// }
+
+// console.log(obj1, obj2);
+
+// const a = "name";
+// const objs = {
+//     name : 't3h',
+//     1: 'số một',
+//     'tên' : 'trung tâm t3h',
+// }
+
+// console.log(objs)
+
+// for(let a in obj5){
+//     console.log(a);
+//     console.log(obj5[5]);
+//     console.log('---');    
+// }
+
+//tính tổng 2 số truyền vào
+
+// function add(a,b){
+//     let sum = a+b;
+//     return sum;
+// }
+
+// let result1 = add(10, 29);
+// console.log(result1);
+
+// khai báo môtj funtion nhận đầu vào là chuối name
+function ham1(minh){
+    console.log('hello world ' + minh);
+}
+ham1('lan anh')
+//thực hiện in ra màn hình hello world + name
+
+// khai báo 1 funstion nhận đầu vào là 2 số a, b
+function sum2(a,b){
+    return (a + b)**2;
+}
+const number = sum2(2,3);
+console.log(sum2(2,3));
+// đầu ra là kết quả của phép tính (a+b)^2
--- Lesion10_2/exemple/exem.js
+++
@@ -1,7 +0,0 @@
-const obj1 = {};
-const obj2 = {
-    Name : "minh",
-    address : "hungw yên"
-}
-
-console.log(obj1, obj2);
